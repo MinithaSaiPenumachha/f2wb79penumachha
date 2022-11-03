0e0c8ea896978811d284b9fa9a5aa8ae5c4233b3
Minitha Sai Penumachha <minipenumatcha8@gmail.com>
2022-11-02 19:43:12 -0500

    changed app

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/tennis', usersRouter);
+app.use('/tennis', tennisRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
