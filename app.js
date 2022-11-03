59fe40b2a4ec1355ea16e1d038900d842fe17a93
Minitha Sai Penumachha <minipenumatcha8@gmail.com>
2022-11-02 20:31:32 -0500

    changes performed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tennisRouter = require('./routes/tennis');
 var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tennis', tennisRouter);
 app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selector);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
