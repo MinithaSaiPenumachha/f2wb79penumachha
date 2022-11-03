0f37d6eec8cbccddfd27eefcae5ea014b3d935df
Minitha Sai Penumachha <minipenumatcha8@gmail.com>
2022-11-02 20:20:29 -0500

    changes made

--- routes/tennis.js
+++ routes/tennis.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function (req, res, next) {
-    res.render('Tennis',{title:'Search Results Tennis'});
+    res.render('tennis',{title:'Search Results'});
 });
 
 module.exports = router;
